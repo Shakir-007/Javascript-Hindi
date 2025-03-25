7b29d32570ab0fd48c975f319e4cb77b1c615a7c
Mohd Shakir <56887751+Shakir-007@users.noreply.github.com>
2025-03-25 05:38:26 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 180478
+let accountEmail = "shakir@gmail.com"
+var accountPassword = "123456"
+accountCity = "Jaipur"
+let accountState;
+
+//accountId = 121212   not allowed
+
+// prefer not to use var variable in js also dont use variable without let or const keywords
+accountEmail = "funny@gmail.com"
+accountPassword = "21212121"
+accountCity = "Delhi"
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
